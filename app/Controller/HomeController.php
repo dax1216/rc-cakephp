971c441d0e5513aaba50b15de90b91cc40975e9c
Dax Panganiban <p_dax2003@yahoo.com>
2013-07-25 16:37:46 +0800

    New stuff

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('AppController', 'Controller');
+
+/**
+ * Static content controller
+ *
+ * Override this controller by placing a copy in controllers directory of an application
+ *
+ * @package       app.Controller
+ * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
+ */
+class HomeController extends AppController {
+
+
+}
